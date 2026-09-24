225518804e502fcb7432dee2f79ae6c021aa48ef
MiladRepo <milad.anaee@gmail.com>
2026-09-24 22:15:12 +0200

    Refactoring class

--- src/MorseCode.java
+++ src/MorseCode.java
@@ -1,32 +1,19 @@
 import java.util.HashMap;
-import java.util.Map;
 
 public class MorseCode {
     private static HashMap<String, String> _map = new HashMap<String, String>(){{
-        put("A", ".-");
-        put("B", "-...");
-        put("C", "-.-.");
-        put("D", "-..");
-        put("E", ".");
-        put("F", "..-.");
-        put("G", "--.");
-        put("H", "....");
-        put("I", "..");
-        put("J", ".---");
-        put("K", "-.-");
-        put("L", ".-..");
-        put("M", "--");
-        put("N", " -.");
-        put("O", "---");
-        put("P", ".--.");
-        put("Q", "--.-");
-        put("R", ".-.");
-        put("S", "...");
-        put("T", "-");
-        put("V", "...-");
-        put("W", ".--");
-        put("X", "-..-");
-        put("Y", "-.--");
+        put("A", ".-"); put("B", "-..."); 
+        put("C", "-.-."); put("D", "-..");
+        put("E", "."); put("F", "..-.");
+        put("G", "--."); put("H", "....");
+        put("I", ".."); put("J", ".---");
+        put("K", "-.-"); put("L", ".-..");
+        put("M", "--"); put("N", " -.");
+        put("O", "---"); put("P", ".--.");
+        put("Q", "--.-"); put("R", ".-.");
+        put("S", "..."); put("T", "-");
+        put("V", "...-"); put("W", ".--");
+        put("X", "-..-"); put("Y", "-.--");
         put("Z", "--..");
     }};
 
